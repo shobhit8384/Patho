bccd3231453920e87636e2550a214c66409e6cc2
shobhit8384 <48446998+shobhit8384@users.noreply.github.com>
2023-07-08 13:52:23 +0530

    Add files via upload

---
+++ billing/src/main/java/com/amrit/pathology/BillingApplication.java
@@ -0,0 +1,14 @@
+package com.amrit.pathology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class BillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingApplication.class, args);
+	}
+
+}
